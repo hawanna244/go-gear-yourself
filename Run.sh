e488a2566a4d758b85d6d0415d47e3ac7fa8b23e
cpi <cpi@clockworkpi>
2019-08-31 17:57:52 +0200

    Restructured application folder. Included Processing and working runscript. We can start developing the game now.

---
+++ Run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# This file has been generated.
+# Change at your own risk.
+SKETCH_NAME=GoGearYourself
+export DISPLAY=:0;
+$(pwd)/processing-3.5.3/processing-java --sketch=GoGearYourself --present
