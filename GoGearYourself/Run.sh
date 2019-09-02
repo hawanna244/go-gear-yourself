b57284d26fb8b7072726b412876772a375b44de1
Felix Bukatz <>
2019-09-02 10:36:09 +0200

    Changing project structure again. Added Loading Screen image.

--- GoGearYourself/Run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# This file has been generated.
-# Change at your own risk.
-SKETCH_NAME=GoGearYourself
-export DISPLAY=:0; processing-3.5.3/processing-java --sketch=GoGearYourself --present
